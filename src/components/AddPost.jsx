b4ed67524ef3d7a9cfb8c54acfb57bbd02789076
unknown <2001020037@student.umrah.ac.id>
2022-11-15 23:01:28 +0700

    Modal React 2

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -66,23 +66,3 @@ const AddPost = () => {
 };
 
 export default AddPost;
-
-//   var modal = document.getElementById("MainPopUp");
-
-// var btn = document.getElementById("ButtonPopUp");
-
-// var span = document.getElementsByClassName("close")[0];
-
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
